0f80e1b039875e1278b3975862d1e1c73138b31e
dermotmcdonagh <dermotmcdonagh@gmail.com>
2023-11-29 11:52:42 +0000

    Added Dismissable Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-
-    console.log(item);
-  }
+ const [alertvisible, setAlertVisibility] = useState(false);
 
   return <div>
-    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+   {alertvisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+   <Button color="danger" onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>;
 }
 export default App;
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,8 +1,19 @@
+import { ReactNode } from "react";
+
+interface Props {
+children: ReactNode;
+onClose: () => void;
 
 
-const Alert = () => {
+}
+
+const Alert = ({ children, onClose } : Props) => {
   return (
-    <div>Alert</div>
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button type="button" className="close" onClick={onClose} data-dismiss="alert" aria-label="Close"></button>
+
+    </div>
   )
 }
 
